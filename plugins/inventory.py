98782dfee5fb2773d57818b889a3a999cf31c4f8
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-04-12 20:52:21 +0500

    Version 0.6

--- plugins/inventory.py
+++ plugins/inventory.py
@@ -1,8 +1,7 @@
 from os.path import exists
 from base64 import b64encode, b64decode
-from typing import List
-
-from cocos.layer import Layer
+from cocos.text import Label
+from cocos.layer import ColorLayer
 from cocos.sprite import Sprite
 
 def _encode(string:str) -> str:
@@ -11,9 +10,10 @@ def _encode(string:str) -> str:
 def _decode(string:str) -> str:
     return b64decode(bytes(string, 'utf-8')).decode('utf-8')
 
-class Inventory():
+class Inventory(ColorLayer):
 
     def __init__(self):
+        super().__init__((255, 255, 255, 150), width=640, height=480)
         if not exists('inventory.list'):
             with open('inventory.list', 'w', encoding='utf-8') as inventory:
                 inventory.write(str(
@@ -22,6 +22,13 @@ class Inventory():
                         for string in ['0' for i in range(5)]
                     ]
                 ))
+        image = Sprite('source/notebook.jpg')
+        image.position = self.width / 2, self.height / 2
+        self.add(image)
+
+        label = Label(
+            text="\n".join(self._read())
+        )
 
     def _read(self) -> list[str]:
         with open('inventory.list', 'r', encoding='utf-8') as file:
@@ -35,3 +42,4 @@ class Inventory():
                     _list
                 ))
             ))
+
